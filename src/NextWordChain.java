0e64e75dc356cbd08302f9ae68a9e33c3d474629
kyle.rosenthal14 <kylem3r@gmail.com>
2014-05-12 09:05:59 -0700

    finished AutoCom, to start word predict

--- src/NextWordChain.java
+++ src/NextWordChain.java
@@ -1,5 +1,10 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
  * Created by kyle.rosenthal14 on 5/8/14.
  */
 public class NextWordChain {
+    HashMap<String, ArrayList<String>> data;
 }
